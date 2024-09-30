a6ec3ad8b85d8f3a391f5a2cfdd1c8f3c3e3e295
seifsherif1544 <163683742+seifsherif1544@users.noreply.github.com>
2024-09-30 18:21:21 +0300

    Create ansible.sh

---
+++ ansible.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+
+
+echo "------------------------- Showing images & Containers -------------------------------"
+
+docker images
+
+docker ps
+
+
+
+echo "------------------------- Deleting images & Containers -------------------------------"
+
+docker stop $(docker ps -a -q)
+
+
+docker rm $(docker ps -a -q)
+
+docker rmi -f $(docker images -aq)  
+
+
+echo "------------------------- Showing images & Containers -------------------------------"
+
+docker images
+
+docker ps
+
+echo "--------------------------- Bulding image from Dockerfile -----------------------------"
+
+docker build -t ansible-website .
+
+echo "----------------------------- Create Container from image -----------------------------"
+
+docker run --name my-ansible-website -d -p 5000:80 ansible-website
+
+echo "---------------------------------- Showing Web-Site -----------------------------------"
+
+sleep 20s
+
+curl localhost:5000
